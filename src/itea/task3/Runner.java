f9bb67df2afa611f34f625c0272ef3c5d001e8f1
vadimkononenko <vadiskon@gmail.com>
2019-10-26 18:26:21 +0300

    bugfix: not empty class

--- src/itea/task3/Runner.java
+++ src/itea/task3/Runner.java
@@ -8,9 +8,7 @@ public class Runner {
         List<Flowers> flowersF = fs.sell(2,4,5);
 
         flowersF.stream().map(f -> f + ",").forEach(System.out::print);
-
         System.out.println();
-
         List<Flowers> flowersS = fs.sellSequence(2,4,5);
         flowersS.stream().map(f -> f + ",").forEach(System.out::print);
     }
