c04564300fb814ffeddd8225d479a52aaa7e31ba
vadimkononenko <vadiskon@gmail.com>
2019-10-29 17:06:58 +0200

    Bugfix: was reworked test of method calculate

--- src/itea/task4/calculator/Runner.java
+++ src/itea/task4/calculator/Runner.java
@@ -2,10 +2,7 @@ package itea.task4.calculator;
 
 public class Runner {
     public static void main(String[] args) {
-
         Service s = new Service();
-        System.out.println(s.calculate('&',2,4));
-
-
+        System.out.println(s.calculate(3,5,'√'));
     }
 }
